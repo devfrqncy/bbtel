3ad20f7937e16ccb6104545f75d12b7e960481aa
devfrqncy <unconfigured@null.spigotmc.org>
2020-03-16 12:12:17 +0100

    utf8mb4

--- src/PDO.php
+++ src/PDO.php
@@ -8,7 +8,8 @@ class PDOdb extends output{
         $options = [
             PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC //make the default fetch be an associative array
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
         ];
         try {
             $pdo = new PDO($dns, $username, $password, $options);
